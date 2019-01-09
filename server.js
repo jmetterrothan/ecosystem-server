bcc070003b110132271c43be00ee5905c7a8b11f
Flroian <zobeleflorian@gmail.com>
2019-01-09 15:10:16 +0100

    feat: socket io

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const WebSocketServer = require('ws').Server;
-const wss = new WebSocketServer({ port: 4200 });
+const uniqid = require('uniqid');
 
-wss.on('connection', ws => {
+const http = require('http').createServer().listen(4200, 'localhost');
+const io = require('socket.io').listen(http);
+
+io.on('connection', ws => {
+
+  // send unique id when user connected
+  const uid = uniqid();
+  io.emit('user_id', uid);
 
   ws.on('message', message => {
     // console.log(message);
+  });
+
+  ws.on('broadcast', players => {
+    ws.broadcast.emit('update_position', ...players);
   })
 
+  ws.on('disconnect', () => console.log('user disconnected'));
 
-  ws.on('close', () => {
-    console.log('user déconnecté');
-  })
-
-})
+});
